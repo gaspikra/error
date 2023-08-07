6e85b5dfb786a8ceffe786bf4e497d7060a2d301
gaspikra <133513044+gaspikra@users.noreply.github.com>
2023-08-07 18:48:24 -0300

    Add files via upload

---
+++ subida_datos.sql
@@ -0,0 +1,33 @@
+USE hoteles;
+INSERT INTO inquilino (id,nombre,apellido,dni,cant_acomp) VALUES
+(null, 'juan','lopez',40779102,0),
+(null,'pedro','perez',32810481,2),
+(null,'tomas','chari',37019372,3),
+(null,'marcos','gonzales',40981614,0),
+(null,'felipe','dasetto',21712901,2),
+(null,'lucas','herrera',39729001,4),
+(null,'juan','pazzeri',30302819,3),
+(null,'kevin','romero',39095810,1),
+(null,'marcelo','herrera',29018314,3),
+(null,'sandra','velli',41906413,1),
+(null,'constanza','zerip',32096799,4),
+(null,'valentin','torres',29753088,2),
+(null,'marcela','diaz',27018375,4),
+(null,'pamela','kirze',27403717,3),
+(null,'rosaa','wellik',42095614,4);
+INSERT INTO habitaciones (id,numero_hab,nombre,capacidad,estado,precio) VALUES
+(null,1,'normal',4,'ocupada',4800),
+(null,2,'normal',4,'libre',4800),
+(null,3,'normal',4,'libre',4800),
+(null,4,'normal',4,'libre',4800),
+(null,5,'grande',6,'libre',7800),
+(null,6,'grande',6,'libre',7800),
+(null,7,'grande',6,'libre',7800),
+(null,8,'grande',6,'ocupada',7800),
+(null,9,'doble',2,'ocupada',3500),
+(null,10,'doble',2,'ocupada',3600),
+(null,11,'doble',2,'ocupada',3600),
+(null,12,'grande',6,'libre',7800),
+(null,13,'normal',4,'libre',4800),
+(null,14,'normal',4,'ocupada',4800),
+(null,13,'triple',3,'libre',4000);
